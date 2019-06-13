5fb0c39c1a567f0a12ccd36409c50814b81dfe92
slinskiy <slinskiy@gmail.com>
2019-06-13 10:52:51 -0700

    Add files via upload

---
+++ basics.py
@@ -0,0 +1,28 @@
+if True:
+    print 'hello'
+    print 'world'
+
+
+# single line comment
+
+"""
+Multi
+line
+comments
+"""
+
+
+variable_name = 'Stan'
+num0 = 2
+num1 = 3
+num2 = 7
+
+num4 = num0 * num1
+
+# Multi line variables
+
+long_name = "This is " + "an example " \
++ "of a variable that "\
++ "takes more than one line"
+
+print long_name
